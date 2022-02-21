caf2c886de892a95a9f34150ab464c7994d6115b
Ahsanadam <adamebadul@gmail.com>
2022-02-21 15:08:07 +0100

    Add functions to Student,StudentRest and StudentService

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -1,7 +1,59 @@
 package se.iths.rest;
 
 
+import se.iths.entity.Student;
+import se.iths.service.StudentService;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("student")
+@Consumes(MediaType.APPLICATION_JSON) // Våran REST applikation kräver och tar emot i JSON format.
+@Produces(MediaType.APPLICATION_JSON) // Våran REST applikation skickar ut svaret i JSON format.
 public class StudentRest {
+
+    @Inject // Injicerar så att man kan använda sig utav StudentService klassen i StudentRest
+    StudentService studentService;
+
+    @Path("new")
+    @POST // Denna annotationen är tillför CREATE i REST
+    public Response createStudent(Student student){
+        studentService.createStudent(student);
+        return Response.ok(student).build();
+
+    }
+
+    @Path("update")
+    @PUT
+    public Response updateStudent(Student student){
+        studentService.updateStudent(student);
+        return Response.ok(student).build();
+    }
+
+    @Path("{id}")
+    @GET
+    public Student getStudent(@PathParam("id") Long id){
+
+        return studentService.findStudentById(id);
+    }
+
+    @Path("getall")
+    @GET
+    public List<Student> getAllStudents(){
+
+        return studentService.getAllStudents();
+    }
+
+    @Path("delete/{id}")
+    @DELETE
+    public Student deleteStudent(@PathParam("id") Long id){
+
+        return studentService.deleteStudent(id);
+    }
+
 
 
 }
--- src/main/java/se/iths/service/StudentService.java
+++ src/main/java/se/iths/service/StudentService.java
@@ -1,7 +1,46 @@
 package se.iths.service;
 
 
-public class StudentService {
+import se.iths.entity.Student;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Transactional /* Denna annotationen talar om att klassen under interagerar med databasen.
+ För att slippa göra en egen transaktion med begin() och close() etc. */
+public class StudentService { /* Denna applikationen kontrollerar flödet av information som kommer in,
+fungerar som en controller kan man säga*/
+    @PersistenceContext
+    EntityManager entityManager; // EntityManager sköter interaktionen med databasen som heter h2
+
+    public void createStudent(Student student){
+        entityManager.persist(student);
+    }
+
+    public void updateStudent(Student student){
+
+        entityManager.merge(student);
+    }
+
+    public Student findStudentById(Long id){
+    return entityManager.find(Student.class,id);
+
+    }
+
+    public List<Student> getAllStudents(){
+        return entityManager.createQuery("SELECT s FROM Student s",Student.class).getResultList();
+    }
+
+
+    public Student deleteStudent(Long id) {
+        Student foundStudent = entityManager.find(Student.class, id);
+        entityManager.remove(foundStudent);
+        return foundStudent;
+    }
+
 
 
 }
